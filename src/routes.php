6d6c3a086046c0dad01355da5bd67e33c2908522
farshid <feri.viper@gmail.com>
2020-12-21 16:03:45 +0330

    sign tag for test my fork

--- src/routes.php
+++ src/routes.php
@@ -1,4 +1,17 @@
 <?php
+
+/*
+------------------ This Project forked for Mobisoft ---------------------
+                 __  __       _     _  _____        __ _   
+                |  \/  |     | |   (_)/ ____|      / _| |  
+                | \  / | ___ | |__  _| (___   ___ | |_| |_ 
+                | |\/| |/ _ \| '_ \| |\___ \ / _ \|  _| __|
+                | |  | | (_) | |_) | |____) | (_) | | | |_ 
+                |_|  |_|\___/|_.__/|_|_____/ \___/|_|  \__|
+
+-------------------------------------------------------------------------
+*/
+
 Route::group(['namespace'=>'Ipecompany\Smsirlaravel\Controllers','middleware'=>config('smsirlaravel.middlewares')], function (){
 	Route::get(config('smsirlaravel.route'),'SmsirlaravelController@index')->name('sms-admin');
 	Route::get('sms-admin/{log}/delete','SmsirlaravelController@delete')->name('deleteLog');
